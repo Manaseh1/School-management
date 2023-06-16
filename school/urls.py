f208ed4de25ee32b968f62986741cbe83acec0b7
Manaseh1 <manasehnjoroge7@gmail.com>
2023-06-16 20:20:44 +0300

    Revert "First commit on school management"
    This reverts commit 2f69cf715de82abfab522e655a3ef8786087610a.

--- school/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import  register,signin,profile,signout
-urlpatterns = [
-    path('',register,name='signup'),
-    path('signin/',signin,name ='signin'),
-    path('profile/',profile,name ="profile"),
-    path('logout',signout,name ='logout'),
-]
